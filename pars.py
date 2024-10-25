9bc98a7655fda0a6661fdc0950b7b4880bc393f4
cxExtazy <eantonicev@gmail.com>
2024-10-26 01:55:04 +0300

    2

--- pars.py
+++ pars.py
@@ -11,41 +11,39 @@ folder_path = 'РЖД train/Выгрузка_маркетинговые спис
 
 def get_file_type():
     files = os.listdir(folder_path)
-    # отдельно для некоторых столбцов
-    # убрать None
     for file in files:
         if file.endswith(".xls"):
             parse_xls(file)
             continue
-            pass
+            # pass
         if file.endswith(".pdf"):
             parse_pdf(file)
             continue
-            pass
+            # pass
         if file.endswith(".docx"):
             parse_docx(file)
             continue
-            pass
+            # pass
 
 def parse_xls(filename: str):
+    new_filename = filename.replace(".xls", "")
     file = pd.read_excel(f"{folder_path}/{filename}")
-    rows = file.loc
     lst = []
     for row in file.itertuples(index=False):
         rw = []
         for r in row:
             rw.append(r)
         lst.append(rw)
-    with open('data_from_xlsx.txt', "w", encoding="UTF-8") as f:
+    with open(f'{new_filename}_xls.txt', "w", encoding="UTF-8") as f:
         f.write(str(lst))
 
 def parse_pdf(filename: str):
-    if os.path.exists("conv_from_pdf.csv"):
-        os.remove("conv_from_pdf.csv")
+    new_filename = filename.replace(".pdf", "")
+    if os.path.exists(f"{new_filename}_pdf.csv"):
+        os.remove(f"{new_filename}_pdf.csv")
     start_time = time.time()
-    print("started")
-    tabula.convert_into(f"{folder_path}/{filename}", "conv_from_pdf.csv", output_format="csv", pages='all')
-    file = pd.read_csv("conv_from_pdf.csv")
+    tabula.convert_into(f"{folder_path}/{filename}", f"{new_filename}_pdf.csv", output_format="csv", pages='all')
+    file = pd.read_csv(f"{new_filename}_pdf.csv")
     lst = []
     for row in file.itertuples(index=False):
         rw = []
@@ -53,30 +51,30 @@ def parse_pdf(filename: str):
             rw.append(r)
         lst.append(rw)
     print(len(lst))
-    with open('data_from_pdf.txt', "w", encoding="UTF-8") as f:
+    with open(f'{new_filename}_pdf.txt', "w", encoding="UTF-8") as f:
         f.write(str(lst))
     print(f"time - {time.time()-start_time}sec")
 
 def parse_docx(filename: str):
     start_time = time.time()
+    new_filename = filename.replace(".docx", "")
     doc = Document(f"{folder_path}/{filename}")
-    if os.path.exists("conv_from_docx.csv"):
-        os.remove("conv_from_docx.csv")
-    with open("conv_from_docx.csv", mode='w', newline='', encoding='utf-8') as csvfile:
+    if os.path.exists(f"{new_filename}_docx.csv"):
+        os.remove(f"{new_filename}_docx.csv")
+    with open(f"{new_filename}_docx.csv", mode='w', newline='', encoding='utf-8') as csvfile:
         csv_writer = csv.writer(csvfile)
         for table in doc.tables:
             for row in table.rows:
                 row_data = [cell.text.strip() for cell in row.cells]
                 csv_writer.writerow(row_data)
-    file = pd.read_csv("conv_from_pdf.csv")
+    file = pd.read_csv(f"{new_filename}_docx.csv")
     lst = []
     for row in file.itertuples(index=False):
         rw = []
         for r in row:
             rw.append(r)
         lst.append(rw)
-    print(len(lst))
-    with open('data_from_docx.txt', "w", encoding="UTF-8") as f:
+    with open(f'{new_filename}_docx.txt', "w", encoding="UTF-8") as f:
         f.write(str(lst))
     print(f"time - {time.time()-start_time}sec")
 
